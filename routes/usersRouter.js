c831293610258ea82f08825f0f3636523adf238b
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-21 18:12:27 +0500

    Added Controllers

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {registerUser, loginUser} = require('../controllers/authController');
+
 
 router.get('/', function(req, res) {
     res.send("Hey it's working");
 });
+
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
 
 module.exports = router;
